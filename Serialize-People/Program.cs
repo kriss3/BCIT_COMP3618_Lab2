0b0f0bbc5b5d4b5003de4e894356e813f0a05ab0
Krzysztof Szczurowski <kszczurowski@absolute.com>
2018-01-23 21:25:19 -0800

    Initial commit to Lab1. BCIT COMP3618; XML Serialization.

--- Serialize-People/Program.cs
+++ Serialize-People/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.IO;
-using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Serialize_People
